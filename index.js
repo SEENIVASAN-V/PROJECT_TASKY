ce09fc83c3b0d2d9fbced46b0a7476d98f82d999
seenivasan.ceh@gmail.com <seenivasan.ceh@gmail.com>
2022-11-11 20:36:19 +0530

    updated deleting feature✌

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //this is the container consider as row 
 const taskContainer = document.querySelector(".taskContainer");
 // obtain values form the user
-const globalStorage=[];
-const generateNewCard =(taskData)=>` 
-<div class="col-md-6 col-lg-4 mt-2 " id="${taskData.id}">
+let globalStorage=[];
+const generateNewCard =(taskData)=>`        
+<div class="col-md-6 col-lg-4 mt-2" >
 <div class="card ">
 <div class="card-header d-flex justify-content-end">
-<button type="button" class="btn btn-success mx-2"><i class="fa-solid fa-pencil"></i></button>
-<button type="button" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+<button type="button" class="btn btn-success" mx-2  ><i class="fa-solid fa-pencil"></i></button>
+<button type="button" class="btn btn-danger" id=${taskData.id} onclick="deleteCard.apply(this, arguments)"><i class="fa-solid fa-trash" id=${taskData.id} onclick="deleteCard.apply(this, arguments)" ></i></button>
 </div>
 <img src=${taskData.imageUrl} class="card-img-top" alt="logo">
 <div class="card-body">
@@ -48,7 +48,23 @@ const saveChanges = () => {
   localStorage.setItem("Tasky",JSON.stringify({cards:globalStorage}));
 // alert("your card created successfully!!,welcome to seenisparrow tasky❤️💡");
 };
+const deleteCard=(event)=>{
+  event = window.event;
+  const targetId = event.target.id;
+  const tagName = event.target.tagName;
 
+  globalStorage = globalStorage.filter((cardObject) => cardObject.id !==targetId);
+
+  localStorage.setItem("Tasky", JSON.stringify({cards:globalStorage}));
+
+  if (tagName === "BUTTON"){
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+  }
+  else{
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+  }
+
+};
 
 
 
